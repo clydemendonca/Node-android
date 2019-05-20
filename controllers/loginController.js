e411718a53dd06d6c0b6ac8bad2230e21cc444a9
prathameshkondaskar <prathameshkondaskar@github.com>
2019-05-20 12:50:57 +0530

    login done

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -5,21 +5,31 @@ module.exports = {
 
     toLogin: function (loginObj) {
 
-        var myUser;
-      return  Login.create(loginObj)
+        //     var myUser;
+        //   return  Login.create(loginObj)
+        //         .then(function (result) {
+        //             myUser = result;
+        //             var promiseUser = Signup.findOne({ username: result.username }).exec();
+        //             return promiseUser;
+
+        //         })
+        //     .then(function (user) {
+        //         if (myUser.password === user.password) {
+
+        //             return user;
+        //         }
+
+        //     })
+        //         .catch()
+
+
+        return Signup.findOne({ username: loginObj.username }).exec()
             .then(function (result) {
-                myUser = result;
-                var promiseUser = Signup.findOne({ username: result.username }).exec();
-                return promiseUser;
+                if (loginObj.password === result.password) {
+                    return result;
 
+                }
             })
-        .then(function (user) {
-            if (myUser.password === user.password) {
-
-                return user;
-            }
-
-        })
             .catch()
 
 
